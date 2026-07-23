e7370cf81ded6e14853ee9bca024f966daf4418d
Joseph <josephgonna33@gmail.com>
2026-07-24 00:17:55 +0300

    Fix multi-page build config

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        blog: resolve(__dirname, "blog.html"),
+        company: resolve(__dirname, "company.html"),
+        contact: resolve(__dirname, "contact.html"),
+        platform: resolve(__dirname, "platform.html"),
+        pricing: resolve(__dirname, "pricing.html"),
+        research: resolve(__dirname, "research.html"),
+        solutions: resolve(__dirname, "solutions.html"),
+      },
+    },
+  },
+});
